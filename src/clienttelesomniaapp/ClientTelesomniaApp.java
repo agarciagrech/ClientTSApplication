62cc22f39e7087dbd4faa8f46843648e8d60d2ab
AlbaOVC <albichuo-v_2000@outlook.com>
2022-11-10 15:27:15 +0100

    run

--- src/clienttelesomniaapp/ClientTelesomniaApp.java
+++ src/clienttelesomniaapp/ClientTelesomniaApp.java
@@ -23,12 +23,15 @@ public class ClientTelesomniaApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        
         Parent root = FXMLLoader.load(getClass().getResource("pantallainicial.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
+       
+     
     }
 
     /**
