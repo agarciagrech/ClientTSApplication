372b690dcbc357c5929e5b186a22ea13d9e2e48e
Angela <agarciagrech@gmail.com>
2022-11-10 13:17:29 +0100

    java fx

---
+++ src/clienttelesomniaapp/PatientAccessController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienttelesomniaapp;
+
+/**
+ *
+ * @author agarc
+ */
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PatientAccessController {
+
+    @FXML
+    private Button loginPatient;
+
+}
+
