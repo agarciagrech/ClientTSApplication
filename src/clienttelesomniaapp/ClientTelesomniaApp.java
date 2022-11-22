a7b7588a9727ab8096ac4060f58ab2934ce7d8cc
jaime <jaime@DESKTOP-1TV3UIP>
2022-11-22 12:55:09 +0100

    Pantalla LogIn

--- src/clienttelesomniaapp/ClientTelesomniaApp.java
+++ src/clienttelesomniaapp/ClientTelesomniaApp.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -43,7 +44,23 @@ public class ClientTelesomniaApp extends Application {
        
      
     }
+    
+    @FXML
+    private void logIn(ActionEvent event){
+        URL url = new File("src/clienttelesomniaapp/doctorRegister.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+       //Parent root = FXMLLoader.load(getClass().getResource("inicio.fxml"));
 
+
+        
+        
+        
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
     /**
      * @param args the command line arguments
      */
